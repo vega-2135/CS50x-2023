f102bbabf7f375df0a9c65bcbed4c5ed6ea71578
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2023-10-14 15:39:05 +0200

    Add week 7 practice: favorites

---
+++ week_7/practice/favorites/favorites.sql
@@ -0,0 +1,5 @@
+UPDATE shows SET title = 'How I Met Your Mother' WHERE title LIKE 'How i met your mother';
+UPDATE shows SET title = 'Brooklyn Nine-Nine' WHERE title LIKE 'b%99';
+UPDATE shows SET title = 'Game of Thrones' WHERE title LIKE 'game%';
+UPDATE shows SET title = 'Game of Thrones' WHERE title LIKE 'GoT';
+UPDATE shows SET title = 'Grey''s Anatomy' WHERE title LIKE 'grey%';
