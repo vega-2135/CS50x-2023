190bcd1ebca20c979bf514a88b0e6eb18eb00b07
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2023-10-18 11:50:26 +0200

    Add favorites.sql file

--- week_7/practice/favorites/favorites.sql
+++ week_7/practice/favorites/favorites.sql
@@ -3,3 +3,5 @@ UPDATE shows SET title = 'Brooklyn Nine-Nine' WHERE title LIKE 'b%99';
 UPDATE shows SET title = 'Game of Thrones' WHERE title LIKE 'game%';
 UPDATE shows SET title = 'Game of Thrones' WHERE title LIKE 'GoT';
 UPDATE shows SET title = 'Grey''s Anatomy' WHERE title LIKE 'grey%';
+SELECT * FROM shows ORDER BY title ASC;
+UPDATE shows SET title = 'It''s Always Sunny in Philadelphia' WHERE title =  'Its Always Sunny in Philidelphia';
