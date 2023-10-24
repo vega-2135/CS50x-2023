e5bf203dd8f8d516388ec73a15361d1cdffaab55
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2023-10-24 11:14:02 +0200

    Add practice problems

--- week_7/practice/prophecy/prophecy.py
+++ week_7/practice/prophecy/prophecy.py
@@ -1,6 +1,4 @@
 from cs50 import SQL
-
-from cs50 import SQL
 import csv
 
 
@@ -8,6 +6,7 @@ def main():
     students_names = []
     houses = []
     heads = []
+    relationship = []
 
     # Read csv file
     # Add student's name, house and head to their correnponding list
@@ -18,30 +17,34 @@ def main():
             student_name = row["student_name"]
             house = row["house"]
             head = row["head"]
+            relation = {"student_name" : row["student_name"], "house": row["house"]}
 
             add_student_names(student_name, students_names)
             add_houses(house, houses)
             add_heads(head, heads)
+            add_relationship(relation, relationship)
 
     # Insert data from csv file to tables in roster.db file
     db = SQL("sqlite:///roster.db")
 
-    for student_name in students_names:
-        db.execute("INSERT INTO students(student_name) VALUES (?)", student_name)
+    for i in range(len(students_names)):
+        db.execute("INSERT INTO students(student_name) VALUES (?)", students_names[i])
     
-    for house in houses:
-        db.execute("INSERT INTO houses(house_name) VALUES (?)", house)
+    for i in range(len(houses)):
+        db.execute("INSERT INTO houses(house_name, head) VALUES (?, ?)", houses[i], heads[i])
     
-    for head in heads:
-        db.execute("INSERT INTO houses(head) VALUES (?)", head)
-    
+    # An alternative for the avove loop would be:
     #for house, head in zip(houses, heads):
         #db.execute("INSERT INTO houses(house_name, head) VALUES (?, ?)", house, head)
+    
+    for i in range(len(relationship)):
+        db.execute("INSERT INTO house_assigments(student_id, house_id) VALUES ((SELECT id FROM students WHERE student_name = ?), (SELECT id from houses WHERE house_name = ?))", relationship[i]["student_name"], relationship[i]["house"])
+    
+    print(students_names)
 
 def add_student_names(student_name, students_names):
     if student_name not in students_names:
         students_names.append(student_name)
-
 
 def add_houses(house, houses):
     if house not in houses:
@@ -50,6 +53,10 @@ def add_houses(house, houses):
 def add_heads(head, heads):
     if head not in heads:
         heads.append(head)
+
+def add_relationship(relation, relationship):
+    if relation not in relationship:
+        relationship.append(relation)
         
 if __name__ == "__main__":
     main()
--- week_7/practice/prophecy/schema.sql
+++ week_7/practice/prophecy/schema.sql
@@ -15,7 +15,20 @@ CREATE TABLE houses (
 
 CREATE TABLE house_assigments (
     student_id INTEGER,
-    houses_id INTEGER,
+    house_id INTEGER,
     FOREIGN KEY (student_id) REFERENCES students(id)
-    FOREIGN KEY (houses_id) REFERENCES houses(id)
+    FOREIGN KEY (house_id) REFERENCES houses(id)
 );
+
+select * from houses where house_name = 'Slytherin';
+
+select id from houses where house_name = 'Slytherin';
+
+select * from house_assigments where house_id = 1;
+
+select student_id from house_assigments where house_id = 1;
+
+select student_id from house_assigments where house_id = (select id from houses where house_name = 'Slytherin');
+
+select student_name from students where id in (select student_id from house_assigments where house_id = (select id from houses wh
+ere house_name = 'Slytherin'));
